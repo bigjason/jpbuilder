5372907af6a0a32b9f8f9bb0489e786893a8e9c5
edogawaconan <me@myconan.net>
2014-08-20 10:42:40 +0900

    Add comment to the start of javascript response to block rosetta flash.

--- lib/jpbuilder-handler.rb
+++ lib/jpbuilder-handler.rb
@@ -16,7 +16,7 @@ class JPbuilderHandler
         result = result.each_char.to_a.map { |chr| chr.ord > 1000 ? "\\\\u\#{"%4.4x" % chr.ord}" : chr }.join
         callback = params[:callback] || JPbuilderHandler.default_callback
         if callback.present?
-          "\#{callback}(\#{result});"
+          "/**/\#{callback}(\#{result});"
         else
           result
         end
